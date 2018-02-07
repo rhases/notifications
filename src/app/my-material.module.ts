0a033a926b2a2e8bfa308f9d6ddff2473fcf108f
Gabriel Rodrigues <gabrielsr@gmail.com>
2018-02-07 19:47:39 -0200

    update version of dependencies to the must updated and fix interface issues

--- src/app/my-material.module.ts
+++ src/app/my-material.module.ts
@@ -2,9 +2,9 @@
 // TODO[developoer] remove not used components
 
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CdkTableModule } from '@angular/cdk/table';
-import 'hammerjs';
 
 import {
     MatAutocompleteModule,
@@ -41,6 +41,41 @@ import {
 } from '@angular/material';
 
 @NgModule({
+    imports: [
+        FlexLayoutModule,
+        CdkTableModule,
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatStepperModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+    ],
     exports: [
         FlexLayoutModule,
         CdkTableModule,
